5d761a3894ea4c8e3f9b54bcd61fe4edaad20070
dorismireya <mir.nereidas@gmail.com>
2017-03-06 20:46:28 -0400

    Listar Compras

--- controllers/compraDataController.php
+++ controllers/compraDataController.php
@@ -4,7 +4,7 @@
 //Listar Compras
 function getListaCompra(){
 	$conexion= Conexion();
-	$query_select = "SELECT l.id_libro AS id_libro, l.categoria_libro AS categoria_libro, l.titulo_libro AS titulo_libro, u.id_usuario AS id_usuario, CONCAT(u.nombres, ' ', u.apellidos) AS usuario, c.id_compra AS id_compra, c.fecha_compra AS fecha_compra FROM libro l, usuario u, compra c ORDER BY c.fecha_compra ASC, l.categoria_libro ASC";
+	$query_select = "SELECT l.id_libro AS id_libro, l.categoria_libro AS categoria_libro, l.titulo_libro AS titulo_libro, u.id_usuario AS id_usuario, CONCAT(u.nombres, ' ', u.apellidos) AS usuario, c.id_compra AS id_compra, DATE_FORMAT(c.fecha_compra,'%d/%m/%Y') AS fecha_compra FROM libro l, usuario u, compra c WHERE u.id_usuario = c.id_usuario AND c.id_libro = l.id_libro ORDER BY c.fecha_compra DESC, l.categoria_libro ASC, l.titulo_libro ASC";
 
 	$res_query_select = mysqli_query($conexion, $query_select)or die('Revise su consulta SELECT');
 	cerrar_conexion($conexion);
