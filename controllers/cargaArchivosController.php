9f94df63519667594598f12216b13003e2922bb3
dorismireya <mir.nereidas@gmail.com>
2017-03-06 23:21:32 -0400

    Sube Imagen a la Base de Datos

--- controllers/cargaArchivosController.php
+++ controllers/cargaArchivosController.php
@@ -1,7 +1,12 @@
 <?php
 
+	
+
 	if(!empty($_FILES['archivo']['name'])) {
 		$ruta_archivos = '../public/uploads/';
+		$ruta_archivos_SQL= 'public/uploads/';
+
+		$id_libro = $_REQUEST['id_libro'];
 
 		$nombre = $_FILES['archivo']['name'];
 		$nombre_tmp = $_FILES['archivo']['tmp_name'];
@@ -21,16 +26,28 @@
 			if($_FILES['archivo']['error'] > 0) {
 			  echo 'Error: ' . $_FILES['archivo']['error'] . '<br/>';
 			} else {
-			  echo 'Nombre: ' . $nombre . '<br/>';
-			  echo 'Tipo: ' . $tipo . '<br/>';
-			  echo 'Tamaño: ' . ($tamano) . ' KB<br/>';
-			  echo 'Guardado en: ' . $nombre_tmp;
+			  //echo 'Nombre: ' . $nombre . '<br/>';
+			  //echo 'Tipo: ' . $tipo . '<br/>';
+			  //echo 'Tamaño: ' . ($tamano) . ' KB<br/>';
+			  //echo 'Guardado en: ' . $nombre_tmp;
 
 				if(file_exists( $ruta_archivos . $nombre)) {
 					echo '<br/>El archivo ya existe: ' . $nombre;
 				} else {
 					move_uploaded_file($nombre_tmp, $ruta_archivos . $nombre);
-					echo '<br/>Guardado en:' . $ruta_archivos . $nombre;
+					//echo '<br/>Guardado en:' . $ruta_archivos . $nombre;
+
+					require 'conexionDB.php';
+					$conexion= Conexion();
+
+					$query_update = "UPDATE libro
+						 SET ruta_imagen = '".$ruta_archivos_SQL . $nombre."' WHERE id_libro='$id_libro'";
+
+					mysqli_query($conexion, $query_update) or die('Revise su consulta de actualizacion');
+					cerrar_conexion($conexion);
+
+					echo "<script language='javascript'>alert('Se subio la imagen exitosamente')</script>";
+					echo "<script language='javascript'>window.location='../views/admin/listarLibro.php'</script>";
 				}
 			}
 
