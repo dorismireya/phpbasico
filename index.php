ff25b8bfde8e66c004db1f8beeb7638c0ded044a
dorismireya <mir.nereidas@gmail.com>
2017-03-06 23:58:11 -0400

    ultimo Index Prinipal

--- index.php
+++ index.php
@@ -12,9 +12,7 @@
 		<?php include 'html/cabecera.php'; ?>
 
 		<section class="contenido wrapper">
-			<p>
-				Plantilla base para el curso b&aacute;sico de php.
-			</p>
+			
 		</section>
 		<?php include 'views/admin/listaLibroPrincipal.php'; ?>
 	</body>
